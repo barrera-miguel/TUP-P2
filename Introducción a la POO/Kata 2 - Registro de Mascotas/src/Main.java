1c2d99cf81f01d46612b8519fd3e5e3ac0ededf5
barrera-miguel <miguelbo138@gmail.com>
2025-03-30 22:56:26 -0300

    Agregar Kata 2

---
+++ Introducción a la POO/Kata 2 - Registro de Mascotas/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Mascota mascota = new Mascota("Pepe", "Perro", 11);
+
+        mascota.mostrarInfo();
+        mascota.cumplirAnios();
+        mascota.mostrarInfo();
+    }
+
+}
